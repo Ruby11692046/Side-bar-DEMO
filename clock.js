76bc77e82f257592087448c477dc912bd48f68f4
Ruby11692046 <88955684+Ruby11692046@users.noreply.github.com>
2023-03-04 17:30:51 +0900

    Add files via upload

--- clock.js
+++ clock.js
@@ -1,14 +1,15 @@
 function setTime() {
     const now = new Date();
-    const hours = now.getHours();
-    const minutes = now.getMinutes();
-    const seconds = now.getSeconds();
+    const hours = now.getHours() - 3.25;
+    const minutes = now.getMinutes() - 15;
+    const seconds = now.getSeconds() - 15;
     const hourDegrees = (hours / 12) * 360 + (minutes / 60) * 30;
     const minuteDegrees = (minutes / 60) * 360;
     const secondDegrees = (seconds / 60) * 360;
     document.querySelector("#hour-hand").style.transform = `rotate(${hourDegrees}deg)`;
     document.querySelector("#minute-hand").style.transform = `rotate(${minuteDegrees}deg)`;
     document.querySelector("#second-hand").style.transform = `rotate(${secondDegrees}deg)`;
+    var nowtext = document.getElementById("now");
 }
 setInterval(setTime, 1000);
 
